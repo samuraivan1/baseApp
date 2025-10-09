69ead3aeada16d8f55d0b44b6f0f96464e008535
samuraivan1 <48805699+samuraivan1@users.noreply.github.com>
2025-10-08 19:31:59 -0600

    unauthorized OK

--- src/features/shell/components/Unauthorized/index.tsx
+++ src/features/shell/components/Unauthorized/index.tsx
@@ -1,2 +1,15 @@
-export { default } from './Unauthorized'
-export { default as UnauthorizedPage } from './Unauthorized'
+// src/pages/Unauthorized/index.tsx
+import React from 'react';
+import { unauthorizedText } from './Unauthorized.messages';
+
+const Unauthorized: React.FC = () => {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>{unauthorizedText.title}</h1>
+      <p>{unauthorizedText.description}</p>
+      <a href="/home">{unauthorizedText.backHome}</a>
+    </div>
+  );
+};
+
+export default Unauthorized;
